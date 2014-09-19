0eae362801b6007bf1c997c051adda8223bc5a07
Ben C <ben@chatlounge.net>
2014-09-18 20:35:40 -0700

    Rename extb_unregistered to extb_unidentified.

--- extensions/extb_unidentified.c
+++ extensions/extb_unidentified.c
@@ -1,5 +1,5 @@
 /*
- * Extended ban type "u": Unregistered users.  Bans unregistered users matching the specified n!u@h.
+ * Extended ban type "u": Unidentified users.  Bans unidentified users matching the specified n!u@h.
  * Example: +b $u:*!webchat@*
  *
  * - Ben (Ben @ irc.chatlounge.net, #ChatLounge-Dev @ irc.chatlounge.net)
@@ -16,12 +16,12 @@ static int _modinit(void);
 static void _moddeinit(void);
 static int eb_unregistered(const char *data, struct Client *client_p, struct Channel *chptr, long mode_type);
 
-DECLARE_MODULE_AV1(extb_unregistered, _modinit, _moddeinit, NULL, NULL, NULL, "Ban Unregistered Users with matching hostmask.");
+DECLARE_MODULE_AV1(extb_unidentified, _modinit, _moddeinit, NULL, NULL, NULL, "Ban Unidentified Users with matching hostmask.");
 
 static int
 _modinit(void)
 {
-	extban_table['u'] = eb_unregistered;
+	extban_table['u'] = eb_unidentified;
 
 	return 0;
 }
@@ -32,12 +32,12 @@ _moddeinit(void)
 	extban_table['u'] = NULL;
 }
 
-static int eb_unregistered(const char *data, struct Client *client_p,
+static int eb_unidentified(const char *data, struct Client *client_p,
 		struct Channel *chptr, long mode_type)
 {
 
 	(void)chptr;
-	// $u alone should match all unregistered users.
+	// $u alone should match all unidentified users.
 	if (data == NULL)
 	{
 		return EXTBAN_INVALID;
@@ -46,10 +46,10 @@ static int eb_unregistered(const char *data, struct Client *client_p,
 	else
 	{
 		char buf[BUFSIZE];
-		
+
 		rb_snprintf(buf, BUFSIZE, "%s!%s@%s",
-			client_p->name, client_p->username, client_p->host);		
-		
+			client_p->name, client_p->username, client_p->host);
+
 		if ((match(data, buf) != 0) && EmptyString(client_p->user->suser))
 		{
 			return EXTBAN_MATCH;
