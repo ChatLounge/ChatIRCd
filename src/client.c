888c90f2d5ea7278a79bb5cf0c09e0b039709e5c
Ben C <ben@chatlounge.net>
2015-08-15 02:37:44 -0500

    Backport Charybdis commits 7fc09bc, 7a9a900, 2635cc8, 364e59f, b647efa

--- src/client.c
+++ src/client.c
@@ -193,7 +193,8 @@ free_pre_client(struct Client *client_p)
 	blptr = client_p->preClient->dnsbl_listed;
 	if (blptr != NULL)
 		unref_blacklist(blptr);
-	abort_blacklist_queries(client_p);
+	s_assert(rb_dlink_list_length(&client_p->preClient->dnsbl_queries) == 0);
+
 	rb_bh_free(pclient_heap, client_p->preClient);
 	client_p->preClient = NULL;
 }
@@ -1258,6 +1259,7 @@ exit_unknown_client(struct Client *client_p, struct Client *source_p, struct Cli
 		  const char *comment)
 {
 	delete_auth_queries(source_p);
+	abort_blacklist_queries(source_p);
 	if (source_p->localClient->dnsquery)
 	{
 		delete_resolver_queries(source_p->localClient->dnsquery);
@@ -1686,6 +1688,18 @@ show_ip_conf(struct ConfItem *aconf, struct Client *source_p)
 	}
 	else
 		return 1;
+}
+
+int
+show_ip_whowas(struct Whowas *whowas, struct Client *source_p)
+{
+	if(whowas->flags & WHOWAS_IP_SPOOFING)
+		if(ConfigFileEntry.hide_spoof_ips || !MyOper(source_p))
+			return 0;
+	if(whowas->flags & WHOWAS_DYNSPOOF)
+		if(!IsOper(source_p))
+			return 0;
+	return 1;
 }
 
 /*
