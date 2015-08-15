d91068ee408647eb3e93d15cdd4712574f53e45b
Ben C <ben@chatlounge.net>
2015-08-15 16:33:38 -0500

    Backport Charybdis commits fabc317, cee842a, 9799bea, 362ef2d, 0c2ea0c, 071e376, 1870e06

--- src/newconf.c
+++ src/newconf.c
@@ -989,9 +989,11 @@ conf_end_auth(struct TopConf *tc)
 
 		conf_add_class_to_conf(yy_tmp);
 
-		if (find_exact_conf_by_address("*", CONF_CLIENT, "*"))
+		if ((found_conf = find_exact_conf_by_address("*", CONF_CLIENT, "*")) && found_conf->spasswd == NULL)
 			conf_report_error("Ignoring redundant auth block (after *@*)");
-		else if (find_exact_conf_by_address(yy_tmp->host, CONF_CLIENT, yy_tmp->user))
+		else if ((found_conf = find_exact_conf_by_address(yy_tmp->host, CONF_CLIENT, yy_tmp->user)) &&
+				(!found_conf->spasswd || (yy_tmp->spasswd &&
+					0 == irccmp(found_conf->spasswd, yy_tmp->spasswd))))
 			conf_report_error("Ignoring duplicate auth block for %s@%s",
 					yy_tmp->user, yy_tmp->host);
 		else
@@ -1697,7 +1699,6 @@ conf_set_serverhide_links_delay(void *data)
 static void
 conf_set_service_name(void *data)
 {
-	struct Client *target_p;
 	const char *s;
 	char *tmp;
 	int dots = 0;
