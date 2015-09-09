bee562e989645520600c5236a866004632a33c50
Ben C <ben@chatlounge.net>
2015-09-09 04:05:55 -0500

    Make make_override_immune.c actually work - Adapted to use umode +y in lieu of cmode +M.

--- extensions/make_override_immune.c
+++ extensions/make_override_immune.c
@@ -1,3 +1,8 @@
+/* Borrowed from Ircd-seven, adapted to use umode +y in lieu of cmode +M.
+ *
+ * - Ben
+ */
+
 #include "stdinc.h"
 #include "modules.h"
 #include "hook.h"
@@ -7,15 +12,50 @@
 #include "numeric.h"
 #include "chmode.h"
 #include "s_newconf.h"
+#include "s_user.h"
+
+#define CanSetNoKick(x)     (HasPrivilege((x), "oper:cansetnokick"))
 
 static void can_kick(hook_data_channel_approval *);
+static void can_set_umode_y(void *vdata);
 
 mapi_hfn_list_av1 nooperkick_hfnlist[] = {
 	{ "can_kick", (hookfn) can_kick },
+	{ "umode_changed", (hookfn) can_set_umode_y },
 	{ NULL, NULL }
 };
 
-DECLARE_MODULE_AV1(chm_no_oper_kick, NULL, NULL, NULL, NULL, nooperkick_hfnlist, "$Revision$");
+static int
+_modinit(void)
+{
+	/* Add the +y user mode. */
+	user_modes['y'] = find_umode_slot();
+	construct_umodebuf();
+
+	return 0;
+}
+
+static void
+_moddeinit(void)
+{
+	user_modes['y'] = 0;
+	construct_umodebuf();
+}
+
+static void
+can_set_umode_y(void *vdata)
+{
+	hook_data_umode_changed *data = (hook_data_umode_changed *)vdata;
+	struct Client *source_p = data->client;
+
+	if(MyClient(source_p) && source_p->umodes & user_modes['y'] && !CanSetNoKick(source_p))
+	{
+		source_p->umodes &= ~user_modes['y'];
+		sendto_one_notice(source_p, ":*** -- You may not set user mode +y.");
+	}
+
+	return;
+};
 
 static void
 can_kick(hook_data_channel_approval *data)
@@ -24,13 +64,17 @@ can_kick(hook_data_channel_approval *data)
 	struct Client *target_p = data->target;
 	struct Channel *chptr = data->chptr;
 
-	if (target_p->umodes & UMODE_OVERRIDE && data->approved)
+	if (target_p->umodes & user_modes['y'] && data->approved &&
+		!IsService(source_p) && !(source_p->umodes & UMODE_OVERRIDE))
 	{
 		sendto_one_numeric(source_p, ERR_ISCHANSERVICE,
 				"%s %s :User is immune from kick.",
 				target_p->name, chptr->chname);
-		sendto_realops_snomask(SNO_GENERAL, L_NETWIDE, "Overriding KICK from %s on %s in %s (user is immune)",
-			source_p->name, target_p->name, chptr->chname);
+		sendto_one_notice(target_p, ":*** Notice -- %s (%s@%s) tried to kick you from %s",
+				source_p->name, source_p->username, source_p->orighost,
+				chptr->chname);
 		data->approved = 0;
 	}
 }
+
+DECLARE_MODULE_AV1(chm_no_oper_kick, _modinit, _moddeinit, NULL, NULL, nooperkick_hfnlist, "$Revision$");
